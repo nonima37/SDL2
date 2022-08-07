9e3707c46ea9d9347c5c56dd69779d01d453299f
nonima37 <nonima444@protonmail.com>
2022-08-07 22:45:15 +0400

    score

--- sdl/sdl2.go
+++ sdl/sdl2.go
@@ -1,3 +1,7 @@
+// ToDo
+// More efficient screen update []
+// Keeping the score [x]
+// Imperfect AI []
 package main
 
 import (
@@ -7,6 +11,55 @@ import (
 )
 
 const winWidth, winHeight int = 800, 600
+
+var nums = [][]byte{
+	{
+		1, 1, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 0, 1,
+		1, 1, 1,
+	},
+	{
+		0, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+		0, 1, 0,
+	},
+	{
+		1, 1, 1,
+		0, 0, 1,
+		1, 1, 1,
+		1, 0, 0,
+		1, 1, 1,
+	},
+}
+
+func drawScore(startx, starty, size, scoreindex int, pixels []byte) {
+	for i := 0; i < len(nums[scoreindex]); i++ {
+		if nums[scoreindex][i] == 1 {
+			for y := starty; y < starty+size; y++ {
+				for x := startx; x < startx+size; x++ {
+					setPixel(x, y, color{255, 255, 255}, pixels)
+				}
+			}
+		} else if scoreindex > 0 {
+			for y := starty; y < starty+size; y++ {
+				for x := startx; x < startx+size; x++ {
+					setPixel(x, y, color{0, 0, 0}, pixels)
+				}
+			}
+		}
+
+		startx += size
+		if (i+1)%3 == 0 {
+			starty += size
+			startx -= size * 3
+		}
+
+	}
+}
 
 type color struct {
 	r, g, b byte
@@ -18,7 +71,7 @@ type pos struct {
 
 type ball struct {
 	pos
-	radius int
+	radius float32
 	xv     float32
 	yv     float32
 	color  color
@@ -28,69 +81,101 @@ func (ball *ball) draw(pixels []byte) {
 	for y := -ball.radius; y < ball.radius; y++ {
 		for x := -ball.radius; x < ball.radius; x++ {
 			if x*x+y*y < ball.radius*ball.radius {
-				setPixel(int(ball.x)+x, int(ball.y)+y, color{255, 255, 255}, pixels)
+				setPixel(int(ball.x+x), int(ball.y+y), color{255, 255, 255}, pixels)
 			}
 		}
 	}
 }
 
+func getCenter() pos {
+	return pos{float32(winWidth) / 2, float32(winHeight) / 2}
+}
+
 // update pos and collision detection
-func (ball *ball) update(paddle1, paddle2 *paddle) {
+func (ball *ball) update(paddle1, paddle2 *paddle, pixels []byte) {
 	ball.x += ball.xv
 	ball.y += ball.yv
 
-	if int(ball.y)-ball.radius < 0 || int(ball.y)+ball.radius > winHeight {
+	if ball.y-ball.radius < 0 || int(ball.y+ball.radius) > winHeight {
 		ball.yv = -ball.yv
 	}
 
-	if ball.x < paddle1.x || ball.x > paddle2.x {
-		ball.x = 300
-		ball.x = 300
+	if ball.x < paddle1.x {
+		paddle2.score++
+		ball.pos = getCenter()
 	}
 
-	if ball.x-float32(ball.radius) < float32(paddle1.x)+float32(paddle1.w/2) {
-		if int(ball.y) >= int(paddle1.y)-paddle1.h/2 && int(ball.y) <= int(paddle1.y)+paddle1.h/2 {
+	if ball.x-ball.radius < paddle1.x+paddle1.w/2 {
+		if ball.y >= paddle1.y-paddle1.h/2 && ball.y <= paddle1.y+paddle1.h/2 {
 			ball.xv = -ball.xv
 		}
 	}
 
-	if ball.x+float32(ball.radius) > float32(paddle2.x)-float32(paddle2.w/2) {
-		if int(ball.y) >= int(paddle2.y)-paddle2.h/2 && int(ball.y) <= int(paddle2.y)+paddle1.h/2 {
+	if ball.x+ball.radius > paddle2.x-paddle2.w/2 {
+		if ball.y >= paddle2.y-paddle2.h/2 && ball.y <= paddle2.y+paddle1.h/2 {
 			ball.xv = -ball.xv
+		}
+	}
+}
+
+func (ball *ball) clear(pixels []byte) {
+	//sdasdas
+	for y := -ball.radius; y < ball.radius; y++ {
+		for x := -ball.radius; x < ball.radius; x++ {
+			if x*x+y*y < ball.radius*ball.radius {
+				setPixel(int(ball.x+x), int(ball.y+y), color{0, 0, 0}, pixels)
+			}
 		}
 	}
 }
 
 type paddle struct {
 	pos
-	w     int
-	h     int
+	w     float32
+	h     float32
+	speed float32
 	color color
+	score int
 }
 
 // x and y are the middle points
 func (paddle *paddle) draw(pixels []byte) {
-	startX := int(paddle.x) - paddle.w/2
-	startY := int(paddle.y) - paddle.h/2
+	startX := int(paddle.x - paddle.w/2)
+	startY := int(paddle.y - paddle.h/2)
 
-	for y := 0; y < paddle.h; y++ {
-		for x := 0; x < paddle.w; x++ {
-			setPixel(startX+x, startY+y, paddle.color, pixels)
+	for y := startY; y < startY+int(paddle.h); y++ {
+		for x := startX; x < startX+int(paddle.w); x++ {
+			setPixel(x, y, paddle.color, pixels)
 		}
 	}
 }
 
-func (paddle *paddle) update(keyState []uint8, pixels []byte) {
-	if keyState[sdl.SCANCODE_UP] != 0 && paddle.y >= float32(paddle.h/2)+10 {
-		paddle.y -= 10
+func (paddle *paddle) update(keyState []uint8) {
+	if keyState[sdl.SCANCODE_UP] != 0 && paddle.y >= paddle.h/2+10 {
+		paddle.y -= paddle.speed
 	}
-	if keyState[sdl.SCANCODE_DOWN] != 0 && paddle.y+float32(paddle.h)/2+10 <= float32(winHeight) {
-		paddle.y += 10
+	if keyState[sdl.SCANCODE_DOWN] != 0 && paddle.y+paddle.h/2+10 <= float32(winHeight) {
+		paddle.y += paddle.speed
+	}
+}
+
+// this is just the draw() function but let's leave it for now
+func (paddle *paddle) clear(pixels []byte) {
+	startX := int(paddle.x) - int(paddle.w/2)
+	startY := int(paddle.y) - int(paddle.h/2)
+
+	for y := 0; y < int(paddle.h); y++ {
+		for x := 0; x < int(paddle.w); x++ {
+			// why doesn't this work??
+			// pixels[((startY+y)*winWidth + startX + x)] = 200
+			// we'll use this for now
+			setPixel(startX+x, startY+y, color{0, 0, 0}, pixels)
+		}
 	}
 }
 
 func (paddle *paddle) aiUpdate(ball *ball) {
-	if int(ball.y) >= paddle.h/2 && int(ball.y) <= winHeight-paddle.h/2 {
+	if ball.y >= paddle.h/2 && ball.y <= float32(winHeight)-paddle.h/2 {
 		paddle.y = ball.y
 	}
 }
@@ -102,13 +187,6 @@ func setPixel(x, y int, c color, pixels []byte) {
 		pixels[index] = c.r
 		pixels[index+1] = c.g
 		pixels[index+2] = c.b
-	}
-}
-
-func clear(pixels []byte) {
-	// this can be done more efficiently
-	for i := range pixels {
-		pixels[i] = 0
 	}
 }
 
@@ -129,26 +207,33 @@ func main() {
 		return
 	}
 
+	// each pixel takes up 4 bytes
+	// 4th one is fo alpha(?), we don't use it
 	pixels := make([]byte, winWidth*winHeight*4)
 
-	player1 := paddle{pos: pos{100, 100}, w: 20, h: 100, color: color{255, 255, 255}}
-	player2 := paddle{pos: pos{700, 100}, w: 20, h: 100, color: color{255, 255, 255}}
-	ball := ball{pos: pos{x: 50, y: 70}, radius: 10, xv: 10, yv: 10, color: color{255, 255, 255}}
+	player1 := paddle{pos: pos{100, 100}, w: 20, h: 100, speed: 20, color: color{255, 255, 255}, score: 0}
+	player2 := paddle{pos: pos{700, 100}, w: 20, h: 100, speed: 20, color: color{255, 255, 255}, score: 0}
+	ball := ball{pos: pos{x: 150, y: 70}, radius: 10, xv: 6, yv: 6, color: color{255, 255, 255}}
 
 	keyState := sdl.GetKeyboardState()
 
 	for {
+		drawScore(200, 30, 20, player2.score, pixels)
+
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.QuitEvent:
 				return
 			}
 		}
-		clear(pixels)
 
-		player1.update(keyState, pixels)
+		player1.clear(pixels)
+		player2.clear(pixels)
+		ball.clear(pixels)
+
+		player1.update(keyState)
 		player2.aiUpdate(&ball)
-		ball.update(&player1, &player2)
+		ball.update(&player1, &player2, pixels)
 
 		player1.draw(pixels)
 		player2.draw(pixels)
@@ -157,11 +242,11 @@ func main() {
 		tex.Update(nil, pixels, winWidth*4)
 		renderer.Copy(tex, nil, nil)
 		renderer.Present()
+
 		sdl.Delay(16)
 	}
 
 	tex.Destroy()
 	window.Destroy()
 	renderer.Destroy()
-
 }
