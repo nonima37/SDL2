e5876989511b32550c77c29a08f293fde2cf55d2
nonima37 <nonima444@protonmail.com>
2022-08-15 16:09:45 +0400

    _

--- sdl/sdl2.go
+++ sdl/sdl2.go
@@ -1,6 +1,7 @@
 // ToDo
-// More efficient screen update []
-// Keeping the score [x]
+// rewrite the way paddle is being drawn to make it easier to rotate []
+// rotating paddle []
+// collision detection needs some improvements []
 // Imperfect AI []
 package main
 
@@ -11,6 +12,15 @@ import (
 )
 
 const winWidth, winHeight int = 800, 600
+
+type gameState int
+
+const (
+	start gameState = iota
+	play
+)
+
+var state = start
 
 var nums = [][]byte{
 	{
@@ -36,29 +46,35 @@ var nums = [][]byte{
 	},
 }
 
-func drawScore(startx, starty, size, scoreindex int, pixels []byte) {
-	for i := 0; i < len(nums[scoreindex]); i++ {
-		if nums[scoreindex][i] == 1 {
-			for y := starty; y < starty+size; y++ {
-				for x := startx; x < startx+size; x++ {
+func drawScore(pos pos, size, scoreIndex int, pixels []byte) {
+	startX := int(pos.x)
+	startY := int(pos.y)
+
+	for i := 0; i < len(nums[scoreIndex]); i++ {
+		if nums[scoreIndex][i] == 1 {
+			for y := startY; y < startY+size; y++ {
+				for x := startX; x < startX+size; x++ {
 					setPixel(x, y, color{255, 255, 255}, pixels)
 				}
 			}
-		} else if scoreindex > 0 {
-			for y := starty; y < starty+size; y++ {
-				for x := startx; x < startx+size; x++ {
+		} else if scoreIndex > 0 {
+			for y := startY; y < startY+size; y++ {
+				for x := startX; x < startX+size; x++ {
 					setPixel(x, y, color{0, 0, 0}, pixels)
 				}
 			}
 		}
 
-		startx += size
+		startX += size
 		if (i+1)%3 == 0 {
-			starty += size
-			startx -= size * 3
+			startY += size
+			startX -= size * 3
 		}
-
 	}
+}
+
+func lerp(a, b, pct float32) float32 {
+	return a + pct*(b-a)
 }
 
 type color struct {
@@ -77,11 +93,17 @@ type ball struct {
 	color  color
 }
 
-func (ball *ball) draw(pixels []byte) {
+func (ball *ball) drawOrClear(clear bool, pixels []byte) {
+	var curColor color
+	if clear {
+		curColor = color{0, 0, 0}
+	} else {
+		curColor = ball.color
+	}
 	for y := -ball.radius; y < ball.radius; y++ {
 		for x := -ball.radius; x < ball.radius; x++ {
 			if x*x+y*y < ball.radius*ball.radius {
-				setPixel(int(ball.x+x), int(ball.y+y), color{255, 255, 255}, pixels)
+				setPixel(int(ball.x+x), int(ball.y+y), curColor, pixels)
 			}
 		}
 	}
@@ -96,35 +118,40 @@ func (ball *ball) update(paddle1, paddle2 *paddle, pixels []byte) {
 	ball.x += ball.xv
 	ball.y += ball.yv
 
-	if ball.y-ball.radius < 0 || int(ball.y+ball.radius) > winHeight {
+	if ball.y-ball.radius <= 0 || int(ball.y+ball.radius) > winHeight {
 		ball.yv = -ball.yv
 	}
 
-	if ball.x < paddle1.x {
-		paddle2.score++
-		ball.pos = getCenter()
+	if ball.x < 0 {
+		//paddle2.score++
+		//ball.pos = getCenter()
+		ball.pos = pos{140, 140}
+		state = start
+	} else if int(ball.x) > winWidth {
+		//paddle1.score++
+		//ball.pos = getCenter()
+		ball.pos = pos{120, 140}
+		state = start
 	}
 
-	if ball.x-ball.radius < paddle1.x+paddle1.w/2 {
+	// outer side collision detection
+	if ball.x-ball.radius == paddle1.x+paddle1.w/2 {
 		if ball.y >= paddle1.y-paddle1.h/2 && ball.y <= paddle1.y+paddle1.h/2 {
 			ball.xv = -ball.xv
+			// for when the ball gets stuck on the paddle and keeps wiggling
+			ball.x = paddle1.x + paddle1.w/2.0 + ball.radius
+		}
+	} else if ball.x-ball.radius < paddle1.x+paddle1.w/2 && ball.x+ball.radius > paddle1.x-paddle1.w/2 {
+		// bottom/top collision detection
+		if ball.yv < 0 && ball.y-ball.radius <= paddle1.y+paddle1.h/2 {
+			ball.yv = -ball.yv
 		}
 	}
 
 	if ball.x+ball.radius > paddle2.x-paddle2.w/2 {
 		if ball.y >= paddle2.y-paddle2.h/2 && ball.y <= paddle2.y+paddle1.h/2 {
 			ball.xv = -ball.xv
-		}
-	}
-}
-
-func (ball *ball) clear(pixels []byte) {
-	//sdasdas
-	for y := -ball.radius; y < ball.radius; y++ {
-		for x := -ball.radius; x < ball.radius; x++ {
-			if x*x+y*y < ball.radius*ball.radius {
-				setPixel(int(ball.x+x), int(ball.y+y), color{0, 0, 0}, pixels)
-			}
+			ball.x = paddle2.x - paddle2.w/2.0 - ball.radius
 		}
 	}
 }
@@ -139,38 +166,32 @@ type paddle struct {
 }
 
 // x and y are the middle points
-func (paddle *paddle) draw(pixels []byte) {
+func (paddle *paddle) drawOrClear(clear bool, pixels []byte) {
 	startX := int(paddle.x - paddle.w/2)
 	startY := int(paddle.y - paddle.h/2)
 
+	var curColor color
+	if clear {
+		curColor = color{0, 0, 0}
+	} else {
+		curColor = paddle.color
+	}
+
 	for y := startY; y < startY+int(paddle.h); y++ {
 		for x := startX; x < startX+int(paddle.w); x++ {
-			setPixel(x, y, paddle.color, pixels)
+			setPixel(x, y, curColor, pixels)
 		}
 	}
+	numX := lerp(paddle.x, getCenter().x, 0.2)
+	drawScore(pos{numX, 35}, 10, paddle.score, pixels)
 }
 
 func (paddle *paddle) update(keyState []uint8) {
-	if keyState[sdl.SCANCODE_UP] != 0 && paddle.y >= paddle.h/2+10 {
+	if keyState[sdl.SCANCODE_UP] != 0 && paddle.y >= paddle.h/2+paddle.speed {
 		paddle.y -= paddle.speed
 	}
-	if keyState[sdl.SCANCODE_DOWN] != 0 && paddle.y+paddle.h/2+10 <= float32(winHeight) {
+	if keyState[sdl.SCANCODE_DOWN] != 0 && paddle.y+paddle.h/2+paddle.speed <= float32(winHeight) {
 		paddle.y += paddle.speed
-	}
-}
-
-// this is just the draw() function but let's leave it for now
-func (paddle *paddle) clear(pixels []byte) {
-	startX := int(paddle.x) - int(paddle.w/2)
-	startY := int(paddle.y) - int(paddle.h/2)
-
-	for y := 0; y < int(paddle.h); y++ {
-		for x := 0; x < int(paddle.w); x++ {
-			// why doesn't this work??
-			// pixels[((startY+y)*winWidth + startX + x)] = 200
-			// we'll use this for now
-			setPixel(startX+x, startY+y, color{0, 0, 0}, pixels)
-		}
 	}
 }
 
@@ -208,18 +229,16 @@ func main() {
 	}
 
 	// each pixel takes up 4 bytes
-	// 4th one is fo alpha(?), we don't use it
+	// 4th one is for alpha(?), we don't use it tho
 	pixels := make([]byte, winWidth*winHeight*4)
 
-	player1 := paddle{pos: pos{100, 100}, w: 20, h: 100, speed: 20, color: color{255, 255, 255}, score: 0}
-	player2 := paddle{pos: pos{700, 100}, w: 20, h: 100, speed: 20, color: color{255, 255, 255}, score: 0}
-	ball := ball{pos: pos{x: 150, y: 70}, radius: 10, xv: 6, yv: 6, color: color{255, 255, 255}}
+	player1 := paddle{pos: pos{100, 100}, w: 20, h: 100, speed: 10, color: color{255, 255, 255}, score: 0}
+	player2 := paddle{pos: pos{700, 100}, w: 20, h: 100, speed: 10, color: color{255, 255, 255}, score: 0}
+	ball := ball{pos: pos{x: 100, y: 170}, radius: 20, xv: 5, yv: -5, color: color{255, 255, 255}}
 
 	keyState := sdl.GetKeyboardState()
 
 	for {
-		drawScore(200, 30, 20, player2.score, pixels)
-
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.QuitEvent:
@@ -227,18 +246,30 @@ func main() {
 			}
 		}
 
-		player1.clear(pixels)
-		player2.clear(pixels)
-		ball.clear(pixels)
+		player1.drawOrClear(true, pixels)
+		player2.drawOrClear(true, pixels)
+		ball.drawOrClear(true, pixels)
 
 		player1.update(keyState)
 		player2.aiUpdate(&ball)
-		ball.update(&player1, &player2, pixels)
 
-		player1.draw(pixels)
-		player2.draw(pixels)
-		ball.draw(pixels)
+		if state == play {
+			player1.update(keyState)
+			player2.aiUpdate(&ball)
+			ball.update(&player1, &player2, pixels)
+		} else if state == start {
+			if keyState[sdl.SCANCODE_SPACE] != 0 {
+				if player1.score == 3 || player2.score == 3 {
+					player1.score = 0
+					player2.score = 0
+				}
+				state = play
+			}
+		}
 
+		player1.drawOrClear(false, pixels)
+		player2.drawOrClear(false, pixels)
+		ball.drawOrClear(false, pixels)
 		tex.Update(nil, pixels, winWidth*4)
 		renderer.Copy(tex, nil, nil)
 		renderer.Present()
